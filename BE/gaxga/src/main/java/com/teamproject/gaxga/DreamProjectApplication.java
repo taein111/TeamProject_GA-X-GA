71a79b20c68c4727eb6a7ef5c4ee445c0a583e7e
TERRY991223 <chfhrl99@naver.com>
2024-04-18 18:47:50 +0900

    메인애니메이션

--- BE/gaxga/src/main/java/com/teamproject/gaxga/DreamProjectApplication.java
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/DreamProjectApplication.java
@@ -1,15 +1,16 @@
 package com.teamproject.gaxga;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class GaxgaApplication {
-
+public class DreamProjectApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(GaxgaApplication.class, args);
+
+		SpringApplication.run(DreamProjectApplication.class, args);
 	}
 
 }
