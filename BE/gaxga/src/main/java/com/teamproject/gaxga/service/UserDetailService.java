4b8e82db569494ca660e07b416b988ee4849dfa8
TERRY991223 <chfhrl99@naver.com>
2024-04-26 11:12:32 +0900

    타임리프헤더이상함

--- BE/gaxga/src/main/java/com/teamproject/gaxga/service/UserDetailService.java
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/service/UserDetailService.java
@@ -1,20 +1,20 @@
-package com.teamproject.gaxga.service;
-
-import com.teamproject.gaxga.entity.User;
-import com.teamproject.gaxga.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Service;
-
-@RequiredArgsConstructor
-@Service
-public class UserDetailService implements UserDetailsService {
-
-    private final UserRepository userRepository;
-
-    @Override
-    public User loadUserByUsername(String gaId){
-        return userRepository.findByGaId(gaId)
-                .orElseThrow(() -> new IllegalArgumentException((gaId)));
-    }
-}
+//package com.teamproject.gaxga.service;
+//
+//import com.teamproject.gaxga.entity.User;
+//import com.teamproject.gaxga.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.stereotype.Service;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class UserDetailService implements UserDetailsService {
+//
+//    private final UserRepository userRepository;
+//
+//    @Override
+//    public User loadUserByUsername(String gaId){
+//        return userRepository.findByGaId(gaId)
+//                .orElseThrow(() -> new IllegalArgumentException((gaId)));
+//    }
+//}
