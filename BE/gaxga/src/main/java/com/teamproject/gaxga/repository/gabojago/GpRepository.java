09d26309f5a7a4322bd8b551f68f47d75b49c15e
DreamSH-J <jangsen94@gmail.com>
2024-04-26 09:39:44 +0900

    가보자고 백업완료

---
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/repository/gabojago/GpRepository.java
@@ -0,0 +1,12 @@
+package com.teamproject.gaxga.repository.gabojago;
+
+import com.teamproject.gaxga.entity.gabojago.GP;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface GpRepository extends JpaRepository<GP, Long> {
+    @Query("SELECT name FROM GP")
+    List<String> findAllNames();
+}
---
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/repository/gabojago/GrRepository.java
@@ -0,0 +1,15 @@
+package com.teamproject.gaxga.repository.gabojago;
+
+import com.teamproject.gaxga.entity.gabojago.GR;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface GrRepository extends JpaRepository<GR, Long> {
+    @Query("SELECT name FROM GR")
+    List<String> findAllNames();
+
+    @Query("SELECT name FROM GR WHERE id = 2")
+    List<String> findById();
+}
