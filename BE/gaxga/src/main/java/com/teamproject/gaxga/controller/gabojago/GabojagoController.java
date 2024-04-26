39c5c7d50d5a575c030d51434e05984979e71ed7
taein111 <taein4546@naver.com>
2024-04-26 09:44:51 +0900

    20240426 가봤다고 백업

--- BE/gaxga/src/main/java/com/teamproject/gaxga/controller/gabojago/GabojagoController.java
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/controller/gabojago/GabojagoController.java
@@ -1,39 +1,39 @@
-package com.teamproject.gaxga.controller.gabojago;
-
-import com.teamproject.gaxga.repository.gabojago.GpRepository;
-import com.teamproject.gaxga.repository.gabojago.GrRepository;
-import com.teamproject.gaxga.repository.gabojago.GtRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-
-@Slf4j
-@Controller
-public class GabojagoController {
-
-    @Autowired
-    private GrRepository grRepository;
-
-    @Autowired
-    private GtRepository gtRepository;
-
-    @Autowired
-    private GpRepository gpRepository;
-
-    @GetMapping("/gabojago")
-    public String loc(Model model){
-        List<String> location = grRepository.findAllNames();
-        List<String> thema = gtRepository.findAllNames();
-        model.addAttribute("themas", thema);
-        model.addAttribute("locations", location);
-        return "gabojago/gabojago";
-    }
-}
-
-
-
-
+//package com.teamproject.gaxga.controller.gabojago;
+//
+//import com.teamproject.gaxga.repository.gabojago.GpRepository;
+//import com.teamproject.gaxga.repository.gabojago.GrRepository;
+//import com.teamproject.gaxga.repository.gabojago.GtRepository;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//import java.util.List;
+//
+//@Slf4j
+//@Controller
+//public class GabojagoController {
+//
+//    @Autowired
+//    private GrRepository grRepository;
+//
+//    @Autowired
+//    private GtRepository gtRepository;
+//
+//    @Autowired
+//    private GpRepository gpRepository;
+//
+//    @GetMapping("/gabojago")
+//    public String loc(Model model){
+//        List<String> location = grRepository.findAllNames();
+//        List<String> thema = gtRepository.findAllNames();
+//        model.addAttribute("themas", thema);
+//        model.addAttribute("locations", location);
+//        return "gabojago/gabojago";
+//    }
+//}
+//
+//
+//
+//
