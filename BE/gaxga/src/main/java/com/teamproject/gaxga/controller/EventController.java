d4873df7fcdbfeca5c21902dd49ccb7b01fd9bc7
TERRY991223 <chfhrl99@naver.com>
2024-04-29 18:58:45 +0900

    이벤트이동

---
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/controller/EventController.java
@@ -0,0 +1,14 @@
+package com.teamproject.gaxga.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EventController {
+
+    @GetMapping("event")
+    public String event() {
+        return "event/event";
+    }
+}
+
