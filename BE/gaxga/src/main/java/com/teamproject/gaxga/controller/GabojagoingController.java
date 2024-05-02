543df7db1bc9151a2e037b9a11205e1691d63afb
DreamSH-J <jangsen94@gmail.com>
2024-05-02 20:02:04 +0900

    가보자고 상세페이지 시작

--- BE/gaxga/src/main/java/com/teamproject/gaxga/controller/GabojagoingController.java
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/controller/GabojagoingController.java
@@ -7,12 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GabojagoingController {
     @GetMapping("/gabojagoing")
-<<<<<<< HEAD
-    public String showGabojagoing(){
+    public String showGabojagoing() {
         return "public/gabojagoing/gabojagoing";
-=======
-    public String Gabojagoing(){
-        return "private/gabojagoing/gabojagoing";
->>>>>>> 7f143ff5c589f1efe99408f8c5a5c8be2580df71
     }
 }
--- BE/gaxga/src/main/java/com/teamproject/gaxga/controller/gabojago/GabojagoController.java
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/controller/gabojago/GabojagoController.java
@@ -1,9 +1,6 @@
 package com.teamproject.gaxga.controller.gabojago;
 
-import com.teamproject.gaxga.entity.gabojago.GP;
-import com.teamproject.gaxga.repository.gabojago.GpRepository;
-import com.teamproject.gaxga.repository.gabojago.GrRepository;
-import com.teamproject.gaxga.repository.gabojago.GtRepository;
+
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 @ToString
 @Slf4j
 @Controller
