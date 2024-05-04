142badbe5a0ede88ff91ef4bac01b0b01c50065e
taein111 <taein4546@naver.com>
2024-05-04 20:23:33 +0900

    가봤다고 테마 분류 수정중

--- BE/gaxga/src/main/resources/static/js/gab_carousel2.js
+++ BE/gaxga/src/main/resources/static/js/gab_carousel2.js
@@ -1,10 +1,9 @@
 let currentIndex = 0;
-let categoryItems = document.querySelectorAll('.intro .center .list a');
-let themaItmes = document.querySelectorAll('.thema a span');
-let article = document.querySelectorAll('.subject .left a');
+let categoryItems = document.querySelectorAll('.intro .center .list a'); //지역카테고리 목록
+let themaItems = document.querySelectorAll('.thema a');// 테마 카테고리 목록
+let article = document.querySelectorAll('.subject .left a');// 분류할 게시글 목록
 
 const totalItems = categoryItems.length;
-
 
 function showCategory(index) {
     categoryItems.forEach((item) => {
@@ -13,8 +12,7 @@ function showCategory(index) {
     categoryItems[index].style.display = 'inline-block';
 }
 
-
-// 필터링 함수 정의
+// 지역 필터링 함수
 function filterByLocal(filter) {
     article.forEach((locals) => {
         if (filter === "지역") {
@@ -27,7 +25,7 @@ function filterByLocal(filter) {
     });
 }
 
-
+//테마 필터링 함수
 function filterBythema(filter){
     article.forEach((themas)=> {
         if(filter === "*" || filter === themas.getAttribute("data-type2")){
@@ -43,7 +41,7 @@ function prevCategory() {
     currentIndex = (currentIndex - 1 + totalItems) % totalItems;
     showCategory(currentIndex);
     const filter = categoryItems[currentIndex].getAttribute("data-filter"); // 다음 카테고리의 data-filter 속성 값
-    filterByLocal(filter); // 필터링 함수 호출
+    filterByLocal(filter); // 지역 필터링 함수 호출
 }
 
 
@@ -51,20 +49,15 @@ function nextCategory() {
     currentIndex = (currentIndex + 1) % totalItems;
     showCategory(currentIndex);
     const filter = categoryItems[currentIndex].getAttribute("data-filter"); // 다음 카테고리의 data-filter 속성 값
-    filterByLocal(filter); // 필터링 함수 호출
+    console.log(filter);
+    filterByLocal(filter); // 지역필터링 함수 호출
     }
 
-
-themaItmes.forEach((thema)=>{
-    thema.addEventListener("click", ()=>{
-        const filter2 = themaItmes.getAttribute("data-filter"); // 다음 카테고리의 data-filter 속성 값
-        filterBythema(filter2); // 필터링 함수 호출
-    })
-});
-
-
-
-
+function themaClick(){
+    const filter2 = event.currentTarget.getAttribute("data-filter");
+    console.log(filter2);
+    filterBythema(filter2); // 테마 필터링 함수 호출
+}
 
 
 // 페이지 로드 시 첫 번째 카테고리 항목 보이기
