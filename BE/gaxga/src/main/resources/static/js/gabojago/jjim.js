2f7a026bcfd63c0a634f61c235c354fd1f98846e
DreamSH-J <jangsen94@gmail.com>
2024-05-09 10:09:58 +0900

    가보자고 수정중

--- BE/gaxga/src/main/resources/static/js/gabojago/jjim.js
+++ BE/gaxga/src/main/resources/static/js/gabojago/jjim.js
@@ -3,9 +3,9 @@ var jjimAfter = document.querySelector('#jjim_after')
 
 window.addEventListener("load", function() {
     const userId = document.querySelector("#jjim_input").value;
-    const gabojagoId = document.querySelector("#gabojago_id_id").value;
+    const gabojagoId = document.querySelector("#gabojago_id").value;
     const jjimStatus = localStorage.getItem(`jjimStatus_${userId}_${gabojagoId}`);
-    if (jjimStatus === "jjim") {
+    if (jjimStatus === "liked") {
         jjimBefore.style.display = "none";
         jjimAfter.style.display = "inline-block";
     } else {
@@ -16,12 +16,19 @@ window.addEventListener("load", function() {
 
 //좋아요 버튼 이벤트처리
 jjimBefore.addEventListener("click",function(){
+    // 좋아요 상태를 로컬 스토리지에 저장
+    const userId = document.querySelector("#jjim_input")
+    const gabojagoId = document.querySelector("#gabojago_id")
+    localStorage.setItem(`jjimStatus_${userId}_${gabojagoId}`, "liked")
+
     jjimBefore.style.display ="none";
     jjimAfter.style.display="inline-block";
+
     const jjim ={
         userId : document.querySelector("#jjim_input").value,
         gabojagoId: document.querySelector("#gabojago_id").value
     };
+
     console.log(jjim);
     const url = "/api/gabojagoing/jjim"
     fetch(url, {
@@ -37,8 +44,14 @@ jjimBefore.addEventListener("click",function(){
 })
 
 jjimAfter.addEventListener("click", function(){
-    jjimAfter.style.display="none";
-    jjimBefore.style.display="inline-block";
+    // 좋아요 상태를 로컬 스토리지에서 제거
+    const userId = document.querySelector("#jjim_input")
+    const gabojagoId = document.querySelector("#gabojago_id")
+    localStorage.setItem(`jjimStatus_${userId}_${gabojagoId}`);
+
+    // 버튼 표시 변경
+    jjimAfter.style.display="inline-block";
+    jjimBefore.style.display="none";
     const jjim = {
         userId: document.querySelector("#jjim_input").value,
         gabojagoId: document.querySelector("#gabojago_id").value
