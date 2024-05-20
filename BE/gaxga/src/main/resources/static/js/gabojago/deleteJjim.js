0fdca8c79d3216e26aea0b948af98f0c5022bccc
DreamSH-J <jangsen94@gmail.com>
2024-05-20 19:22:16 +0900

    가보자고 마이페이지 삭제 기능 추가

--- BE/gaxga/src/main/resources/static/js/gabojago/deleteJjim.js
+++ BE/gaxga/src/main/resources/static/js/gabojago/deleteJjim.js
@@ -2,8 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // DOMContentLoaded == 문서가 완전히 로드된 후에 스크립트가 실행되도록 함
     const deleteButtons = document.querySelectorAll('#deleteBtn');
 
+    function handleClick() {
+        // 페이지 새로고침
+        window.reload();
+    }
+
     deleteButtons.forEach(button => {
         button.addEventListener('click', function() {
+            event.preventDefault();
             const userId = this.closest('.receiveLikeList').querySelector("#jjim_input").value;
             const gabojagoId = this.closest('.receiveLikeList').querySelector("#gabojago_id").value;
             localStorage.removeItem(`jjimStatus_${userId}_${gabojagoId}`);
@@ -27,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (response.ok) {
                     this.closest('.receiveLikeList').remove();
                 }
+                window.location.reload();
             });
         });
     });
