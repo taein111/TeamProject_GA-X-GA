443cbe1fcfdeaeec4ce98b6a71f1ed8e5448c02b
taein111 <taein4546@naver.com>
2024-05-23 13:53:48 +0900

    가봤다고 수정

--- BE/gaxga/src/main/resources/static/js/form_validation.js
+++ BE/gaxga/src/main/resources/static/js/form_validation.js
@@ -9,22 +9,3 @@ function validateForm() {
 
     return true; // 폼 제출 허용
 }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var fileInput = document.querySelector('input[type="file"]');
-//     var validExtensions = ['jpg', 'jpeg', 'png', 'gif']; // 허용되는 파일 확장자 목록
-//
-//     fileInput.addEventListener('change', function() {
-//         if (fileInput.files.length > 0) {
-//             for (var i = 0; i < fileInput.files.length; i++) {
-//                 var file = fileInput.files[i];
-//                 var fileExtension = file.name.split('.').pop().toLowerCase();
-//                 if (!validExtensions.includes(fileExtension)) {
-//                     alert('이미지 파일만 업로드 가능합니다. (jpg, jpeg, png, gif)');
-//                     fileInput.value = ''; // 파일 선택 초기화
-//                     return; // 추가 처리 중단
-//                 }
-//             }
-//         }
-//     });
-// });
