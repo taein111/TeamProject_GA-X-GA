0b73d01c234ac61145bf8509ae0f14d09675a4a5
taein111 <taein4546@naver.com>
2024-06-03 10:49:44 +0900

    가봤다고 수정

--- BE/gaxga/src/main/java/com/teamproject/gaxga/controller/gabowatdago/CmtApiController.java
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/controller/gabowatdago/CmtApiController.java
@@ -16,26 +16,20 @@ public class CmtApiController {
     //1. 댓글 조회
     @GetMapping("/api/gabowatdago/{gabowatdagoId}/cmts")
     public ResponseEntity<List<CmtDto>> cmts(@PathVariable("gabowatdagoId") Long gabowatdagoId){
-        //서비스에 위임
         List<CmtDto> dtos = cmtService.comments(gabowatdagoId);
-        //결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
     }
     //2. 댓글 생성
     @PostMapping("/api/gabowatdago/{gabowatdagoId}/cmts")
     public ResponseEntity<CmtDto> createCmt(@PathVariable("gabowatdagoId")Long gabowatdagoId,
                                             @RequestBody CmtDto cmtDto){
-        //서비스에 위임
         CmtDto createdDto = cmtService.create(gabowatdagoId, cmtDto);
-        //결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(createdDto);
     }
     //3. 댓글 수정
     @PatchMapping("/api/cmts/{id}")
     public ResponseEntity<CmtDto> updateCmt(@PathVariable("id") Long id, @RequestBody CmtDto cmtDto){
-        //서비스 위임
         CmtDto updatedDto = cmtService.update(id, cmtDto);
-        // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
     }
 
@@ -43,9 +37,7 @@ public class CmtApiController {
     //4. 댓글 삭제
     @DeleteMapping("/api/cmts/{id}")
     public ResponseEntity<CmtDto> deleteCmt(@PathVariable("id") Long id){
-        //서비스 위임
         CmtDto deletedDto = cmtService.delete(id);
-        //결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(deletedDto);
     }
 
